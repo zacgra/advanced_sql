7be27c9e477c6d0798f2c833dd26ae343821fbf3
Zach Grammon <zachgrammon@gmail.com>
2023-11-05 21:12:47 -0700

    Consolidates customer CTEs

--- week_2/proj_1.sql
+++ week_2/proj_1.sql
@@ -10,51 +10,7 @@
 */
 
 with     
-    customers as (
-        /*  Consolidates pre-query customer data */
-        select
-            customer_data.customer_id as id
-            , customer_data.first_name || ' ' || customer_data.last_name as customer_name
-            , customer_address.customer_city
-            , customer_address.customer_state
-        from vk_data.customers.customer_address
-        inner join vk_data.customers.customer_data on 
-            customer_address.customer_id = customer_data.customer_id
-    )
-
-    , customers_affected as (
-        /*  Filters out unaffected customers */
-        select *
-        from customers
-        where 
-            (  
-                (( customer_state = 'KY') and (customer_city ilike '%concord%' 
-                                            or customer_city ilike '%georgetown%' 
-                                            or customer_city ilike '%ashland%' )
-                )  
-                or
-                ( (customer_state = 'CA') and (customer_city ilike '%oakland%' 
-                                                or customer_city ilike '%pleasant hill%' )
-                )
-                or
-                ( (customer_state = 'TX') and (customer_city ilike '%arlington%'
-                                            or customer_city ilike '%brownsville%')
-                )
-            )
-    )
-    
-    , customers_affected_with_locations as (
-        /* Adds to affected customers the the geo location of each customer */
-        select
-            c.*
-            , us.geo_location
-        from customers_affected as c
-        left join vk_data.resources.us_cities as us on
-            upper(trim(c.customer_state)) = upper(trim(us.state_abbr))
-            and upper(trim(c.customer_city)) = upper(trim(us.city_name)) 
-    )
-    
-    , chicago_geolocation as (
+    chicago_geolocation as (
         /* Returns single row table with geo_location of Chicago */
         select geo_location
         from vk_data.resources.us_cities 
@@ -69,16 +25,47 @@ with
         where city_name = 'GARY' and state_abbr = 'IN'
         limit 1
     )
+
+    , customers_affected as (
+        /*  consolidates customer data and filters out unaffected customers
+        */
+        select
+            customer_data.customer_id as id
+            , customer_data.first_name || ' ' || customer_data.last_name as customer_name
+            , ca.customer_city
+            , ca.customer_state
+        from vk_data.customers.customer_address as ca
+        inner join vk_data.customers.customer_data on
+            ca.customer_id = customer_data.customer_id
+        where ((ca.customer_state = 'KY') and (ca.customer_city ilike '%concord%' 
+                                                or ca.customer_city ilike '%georgetown%'
+                                                or ca.customer_city ilike '%ashland%'))
+            or ((ca.customer_state = 'CA') and (ca.customer_city ilike '%oakland%'
+                                                or ca.customer_city ilike '%pleasant hill%'))
+            or ((ca.customer_state = 'TX') and (ca.customer_city ilike '%arlington%'
+                                                or ca.customer_city ilike '%brownsville%'))
+    )
+
+    , customers_affected_with_locations as (
+        /* Adds to affected customers the the geo location of each customer */
+        select
+            customers_affected.*
+            , us.geo_location
+        from customers_affected
+        left join vk_data.resources.us_cities as us on
+            upper(trim(customers_affected.customer_state)) = upper(trim(us.state_abbr))
+            and upper(trim(customers_affected.customer_city)) = upper(trim(us.city_name)) 
+    )
     
     , customers_affected_with_locations_and_distances as (
         /*  Adds to accumulating customers table a column with calculated 
             distance between the customer and supply city
         */
         select
-            cal.*
-            , (st_distance(cal.geo_location, chicago.geo_location) / 1609)::int as chicago_distance_miles
-            , (st_distance(cal.geo_location, gary.geo_location) / 1609)::int as gary_distance_miles
-        from customers_affected_with_locations as cal
+            customers_awl.*
+            , (st_distance(customers_awl.geo_location, chicago.geo_location) / 1609)::int as chicago_distance_miles
+            , (st_distance(customers_awl.geo_location, gary.geo_location) / 1609)::int as gary_distance_miles
+        from customers_affected_with_locations as customers_awl
         cross join chicago_geolocation as chicago
         cross join gary_geolocation as gary
     )
@@ -94,11 +81,11 @@ with
     )
     
 select
-    c.customer_name
-    , c.customer_city
-    , c.customer_state
-    , cp.food_pref_count
-    , c.chicago_distance_miles
-    , c.gary_distance_miles
-from customers_affected_with_locations_and_distances as c
-inner join customer_preferences as cp on c.id = cp.customer_id
+    customers_affected.customer_name
+    , customers_affected.customer_city
+    , customers_affected.customer_state
+    , customer_preferences.food_pref_count
+    , customers_affected.chicago_distance_miles
+    , customers_affected.gary_distance_miles
+from customers_affected_with_locations_and_distances as customers_affected
+inner join customer_preferences on customers_affected.id = customer_preferences.customer_id
