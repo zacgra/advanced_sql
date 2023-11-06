5053cfec9e9938250ba483bf85f60d915682a526
Zach Grammon <zachgrammon@gmail.com>
2023-11-05 19:32:01 -0700

    Update based on sytle guide

--- week_2/proj_1.sql
+++ week_2/proj_1.sql
@@ -1,31 +1,31 @@
+/*
+    I attempted to follow the Brookly Data Co style guide.  Please let me know
+    if something is misaligned with the guide! :) 
+    https://github.com/brooklyn-data/co/blob/main/sql_style_guide.md
+
+    My approach in refactoring was to break each transformation or set of related
+    transformations in to a CTE. (In Brooklyn Data Co they refer to this as a 
+    logical unit of work.) In the final select, I join the customer geo/distance
+    data with customer survey results.
+*/
+
 with     
-    customer_name_and_address as (
-        /*    
-            consolidates pre-query customer data
-        */
-
+    customers as (
+        /*  Consolidates pre-query customer data */
         select
-            c.customer_id,
-            first_name || ' ' || last_name as customer_name,
-            ca.customer_city,
-            ca.customer_state
+            cd.customer_id as id
+            , cd.first_name || ' ' || cd.last_name as customer_name
+            , ca.customer_city
+            , ca.customer_state
         from vk_data.customers.customer_address as ca
-        join vk_data.customers.customer_data c 
-            on ca.customer_id = c.customer_id
-    ),
-    
-    affected_customers as (
-        /*  - filters out unaffected customers
-            - passes the prior related CTEs records along for accumulating results
-              so that we don't recalculate CTEs multiple times
-            - since ilike matches case insensitive, we don't need to worry about
-              case.  Also, our wildcards will match regardless of whitespace
-              before or after our target string so we don't need trim.
-        */
+        inner join vk_data.customers.customer_data cd on 
+            ca.customer_id = cd.customer_id
+    )
 
-        select
-            c.*
-        from customer_name_and_address as c
+    , customers_affected as (
+        /*  Filters out unaffected customers */
+        select c.*
+        from customers as c
         where 
             (  
                 (( c.customer_state = 'KY') and (c.customer_city ilike '%concord%' 
@@ -41,84 +41,64 @@ with
                                             or c.customer_city ilike '%brownsville%')
                 )
             )
-    ),
+    )
     
-    affected_customer_locations as (
-        /*
-            - adds a customer's geo_location based on their city and state
-            - I decided to apply the affected customer before this
-              to avoid processing irrelevant data
-        */
-
-        select 
-            c.*,
-            us.geo_location
-        from affected_customers as c
-        left join vk_data.resources.us_cities as us
-            on upper(trim(c.customer_state)) = upper(trim(us.state_abbr))
-            and upper(trim(c.customer_city)) = upper(trim(us.city_name)) 
-    ),
-
-    chicago_geolocation as (
-        /*
-            create a single row table to retrieve Chicago geo_location
-        */
-
+    , customers_affected_with_locations as (
+        /* Adds to affected customers the the geo location of each customer */
         select
-            geo_location
+            c.*
+            , us.geo_location
+        from customers_affected as c
+        left join vk_data.resources.us_cities as us on
+            upper(trim(c.customer_state)) = upper(trim(us.state_abbr))
+            and upper(trim(c.customer_city)) = upper(trim(us.city_name)) 
+    )
+    
+    , chicago_geolocation as (
+        /* Returns single row table with geo_location of Chicago */
+        select geo_location
         from vk_data.resources.us_cities 
         where city_name = 'CHICAGO' and state_abbr = 'IL'
         limit 1
-    ),
-
-    gary_geolocation as (
-        /*
-            create a single row table to retrieve Gary geo_location
-        */
-
-        select
-            geo_location
+    )
+    
+    , gary_geolocation as (
+        /* Returns single row table with geo_location of Gary */
+        select geo_location
         from vk_data.resources.us_cities 
         where city_name = 'GARY' and state_abbr = 'IN'
         limit 1
-    ),
-
-    affected_customers_with_suppy_store_distance as (
-        /*
-            - calculates the distance between the customer and city
-            - note: each cross join won't create additional rows since there is 
-              only one result in each city-specific table
+    )
+    
+    , customers_affected_with_locations_and_distances as (
+        /*  Adds to accumulating customers table a column with calculated 
+            distance between the customer and supply city 
         */
-
         select
-            c.*,
-            (st_distance(c.geo_location, chicago.geo_location) / 1609)::int as chicago_distance_miles,
-            (st_distance(c.geo_location, gary.geo_location) / 1609)::int as gary_distance_miles
-        from affected_customer_locations as c
+            c.*
+            , (st_distance(c.geo_location, chicago.geo_location) / 1609)::int as chicago_distance_miles
+            , (st_distance(c.geo_location, gary.geo_location) / 1609)::int as gary_distance_miles
+        from customers_affected_with_locations as c
         cross join chicago_geolocation as chicago
         cross join gary_geolocation as gary
-    ),
-
-    customer_preferences as (
-        /* 
-            - customer preferences can be joined last so we aren't passing 
-              non-essential data around
-        */
-        select 
-            cs.customer_id,
-            count(cs.customer_id) as food_pref_count
+    )
+    
+    , customer_preferences as (
+        /*  Creates a table of the number of active survey results for each customer */ 
+        select
+            cs.customer_id
+            , count(*) as food_pref_count
         from vk_data.customers.customer_survey as cs
         where is_active = true
         group by 1
     )
     
 select
-    c.customer_name,
-    c.customer_city,
-    c.customer_state,
-    cp.food_pref_count,
-    c.chicago_distance_miles,
-    c.gary_distance_miles
-from affected_customers_with_suppy_store_distance as c
-left join customer_preferences as cp
-    on c.customer_id = cp.customer_id
+    c.customer_name
+    , c.customer_city
+    , c.customer_state
+    , cp.food_pref_count
+    , c.chicago_distance_miles
+    , c.gary_distance_miles
+from customers_affected_with_locations_and_distances as c
+inner join customer_preferences as cp on c.id = cp.customer_id
